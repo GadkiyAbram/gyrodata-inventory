418cc647bb449b15fe15f8e4aea164aa24c0b0ba
a.s.abramovskiy <a.s.abramovskiy@tmn3.etagi.com>
2022-05-23 15:02:50 +0500

    added files to VSC

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React, {Fragment} from 'react';
+import BatteryTable from "./BatteryTable";
 
 const Dashboard = ({setAuth}) => {
     return (
         <Fragment>
             <h1>Dashboard</h1>
+            <BatteryTable />
             <button
                 onClick={() => setAuth(false)}
             >
