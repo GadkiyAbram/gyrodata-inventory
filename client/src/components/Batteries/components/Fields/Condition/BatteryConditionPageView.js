c0bc5d38fee74fbdf4b0ce6735eb66f437208a81
gadkiyabram <a.s.abramovskiy@tmn3.etagi.com>
2023-03-04 08:54:10 +0500

    Handle User Authorization using AuthStore

---
+++ client/src/components/Batteries/components/Fields/Condition/BatteryConditionPageView.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import useStyles from '../../styles';
+import {condition} from '../../../../enums/batteries';
+
+const BatteryConditionPageView = (props) => {
+    const classes = useStyles();
+
+    const {
+        row,
+        updateCondition
+    } = props;
+
+    return (
+        <div
+            className={classes.avatar}
+            style={{
+                backgroundColor:
+                    ((row.condition === condition.NEW && 'green') ||
+                        (row.condition === condition.USED && 'orange'))
+            }}
+        />
+    )
+}
+
+export default BatteryConditionPageView;
